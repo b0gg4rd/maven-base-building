960d5ca694433231c3582ec2ffd78f04fdc30af0
b0gg4rd <boggard@hotmail.com>
2017-02-14 00:08:15 -0600

    refactor(app): rename main class

--- src/main/java/net/coatli/java/MvnJavaBuildingApplication.java
+++ src/main/java/net/coatli/java/MvnJavaBuildingApplication.java
@@ -4,12 +4,12 @@ import org.joda.time.LocalTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Application {
+public class MvnJavaBuildingApplication {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(MvnJavaBuildingApplication.class);
 
   public static void main(final String[] args) {
-	LOGGER.info("The current local time is: {}", new LocalTime());
+	  LOGGER.info("The current local time is: {}", new LocalTime());
 
     LOGGER.info(new Greeter().sayHello());
   }
