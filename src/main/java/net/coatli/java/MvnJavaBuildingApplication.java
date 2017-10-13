f35241bc8b2becd6989b10de2826dc1f71b3bda3
b0gg4rd <boggard@hotmail.com>
2017-10-12 21:34:20 -0500

    style(app): add end file new line

--- src/main/java/net/coatli/java/MvnJavaBuildingApplication.java
+++ src/main/java/net/coatli/java/MvnJavaBuildingApplication.java
@@ -9,8 +9,10 @@ public class MvnJavaBuildingApplication {
   private static final Logger LOGGER = LoggerFactory.getLogger(MvnJavaBuildingApplication.class);
 
   public static void main(final String[] args) {
-	  LOGGER.info("The current local time is: {}", new LocalTime());
+    LOGGER.info("The current local time is: {}", new LocalTime());
 
     LOGGER.info(new Greeter().sayHello());
   }
+
 }
+
